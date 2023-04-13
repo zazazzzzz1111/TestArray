679e7cb78c27262494dfe055cc3ad17b365374ec
zazazzzzz1111 <zdxfbfh@mail.ru>
2023-04-13 18:39:19 +0400

    Sort array

--- TestArray/Program.cs
+++ TestArray/Program.cs
@@ -1,22 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Runtime.CompilerServices;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TestArray
 {
     internal class Program
     {
+        private const int NmbersCount = 20;
+        private static readonly Random Random = new Random();
+
         static void Main(string[] args)
-        {
-
-
-
-
-
-
-
+        { 
+            var array = Enumerable.Range(0, NmbersCount).Select(_=> Random.Next(10)).ToArray();
+            array = array.OrderBy(x => x).ToArray();
 
 
         }
